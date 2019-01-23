914a490928ea6a57a0e3270ed2cec891347ebfa5
pixlperfect01 <44816788+pixlperfect01@users.noreply.github.com>
2019-01-23 10:30:02 -0600

    Update shapes.js

--- 3D/shapes.js
+++ 3D/shapes.js
@@ -1,1 +1,18 @@
-
+function easy3D(canvas){
+  this.canvas=canvas;
+  this.ctx=canvas.getContext("2d");
+  this.graph=array3D(canvas.width, canvas.height, 1000);
+  this.cube=function(x, y, z, size){
+    
+  }
+}
+function array3D(a, b, c){
+  var out=new Array(a);
+  for(var i=0;i<out.length;i++){
+    out[i]=new Array(b);
+    for(var j=0;j<out[i].length;j++){
+      out[i][j]=new Array(c);
+    }
+  }
+  return out;
+}
